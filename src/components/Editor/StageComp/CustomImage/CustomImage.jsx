a3ca5d22c32fefea7e67172aaf47fcdeec4f6393
Ziv200 <ziv200@gmail.com>
2023-07-24 14:47:23 +0530

    Added X btn to customimage

--- src/components/Editor/StageComp/CustomImage/CustomImage.jsx
+++ src/components/Editor/StageComp/CustomImage/CustomImage.jsx
@@ -1,12 +1,16 @@
 import { Image, Text, Group } from "react-konva";
 import useImage from "use-image";
 import { useAppState, useActions } from "../../../../overmind";
-
+//css
+import classes from "./CustomImage.module.css";
 //dummy data
 import icons from "../../../../assets/Icons";
 
 const CustomImage = ({ data }) => {
   const actions = useActions();
+  const state = useAppState();
+  const editEnabled = state.editPage.editEnable;
+  const selectedObjId = state.editPage.selectedObjId;
   // The dragBoundFunc for restricting drag movement within the stage
   const dragBoundFunc = (pos) => {
     const stageWidth = 1123;
@@ -28,6 +32,19 @@ const CustomImage = ({ data }) => {
       dragBoundFunc={dragBoundFunc}>
       <Image width={100} height={100} image={image} />
       <Text fontSize={14} fontStyle='bold' text={`${data.inputno}. ${data.name}`} />
+      {editEnabled && selectedObjId === data.id && (
+        <Group>
+          <Text
+            className={classes.deletexbtn}
+            onClick={() => alert(123)}
+            id={`obj_${data.id}_remove`}
+            fontSize={30}
+            text='x'
+            x={100}
+            y={-30}
+          />
+        </Group>
+      )}
     </Group>
   );
 };
